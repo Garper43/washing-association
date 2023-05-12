c8c0b2a24a935713e622d767965fa82d3a6f04fd
Garper43 <tarassozansky@gmail.com>
2023-05-11 21:24:26 -0700

    did some work on comparison images

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 const ui = {
     gallery: {
-        image: document.getElementsByClassName('image'),
+        image: document.getElementById('before-image'),
+        slider: document.getElementById('slider'),
     }
 }
+
+ui.gallery.slider.addEventListener('input', (ev) => {
+    ui.gallery.image.style.width = ui.gallery.slider.value + '%';
+});
 

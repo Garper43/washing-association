52707e00c66e2ab0e6c7dd7aa7ee0b72625bf901
Garper43 <tarassozansky@gmail.com>
2023-05-13 23:42:01 -0700

    comparison fecture

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 const ui = {
     gallery: {
-        image: document.getElementById('before-image'),
+        beforeImage: document.getElementById('before-image'),
+        afterImage: document.getElementById('after-image'),
         slider: document.getElementById('slider'),
     }
 }
 
 ui.gallery.slider.addEventListener('input', (ev) => {
-    ui.gallery.image.style.width = ui.gallery.slider.value + '%';
+    console.log(Math.floor(ui.gallery.slider.value/100*ui.gallery.afterImage.offsetWidth) + 'px');
+    ui.gallery.beforeImage.style.width = Math.floor(ui.gallery.slider.value/100*ui.gallery.afterImage.offsetWidth) + 'px';
 });
 

0ddc8c84b94012460f3a601004dd8dfc56eb0c80
Garper43 <tarassozansky@gmail.com>
2023-05-16 23:15:17 -0700

    did some work ig

--- index.js
+++ index.js
@@ -6,6 +6,9 @@ const ui = {
     }
 }
 
+//reset slider
+ui.gallery.slider.value = 50;
+
 ui.gallery.slider.addEventListener('input', (ev) => {
     console.log(Math.floor(ui.gallery.slider.value/100*ui.gallery.afterImage.offsetWidth) + 'px');
     ui.gallery.beforeImage.style.width = Math.floor(ui.gallery.slider.value/100*ui.gallery.afterImage.offsetWidth) + 'px';
